9ffe59b7f08ea0581a34c5e315cc2b3d7d3d441b
Vansh Dhiwar <vansh.dhiwar985@gmail.com>
2025-02-19 14:03:40 +0530

    pokemon fetcher made using API

---
+++ index.js
@@ -0,0 +1,19 @@
+async function fetchData() {
+    try {
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if (!response.ok) {  // ✅ Corrected the property name
+            throw new Error("Could not fetch resource");
+        }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+        imgElement.src = pokemonSprite;
+        imgElement.style.display ="block";
+
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+}
